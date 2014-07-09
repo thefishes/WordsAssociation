83176b59f1640add78362988c25e36ada71d7843
thefishes <ecedogrucu@gmail.com>
2014-07-09 16:30:40 -0400

    fixed synsets and added lemmas

--- WordsAssociation.py
+++ WordsAssociation.py
@@ -3,24 +3,31 @@ import nltk
 from nltk.corpus import wordnet as wn
 
 user_input = raw_input("Start Typing")
+
     
 def getsynsets(user_input):
     
         for synset in (wn.synsets(user_input)):
-            try:
-                print synset
-                print synset.hypernyms() 
-                print synset.hyponyms()
-                print synset.meronyms()
-                print synset.holonyms()
-                print synset.part_meronyms()
-                print synset.sister_terms()
-                print synset.troponyms()
-                print synset.derivationally_related_forms()
-                print synset.inherited_hypernyms()
-            except AttributeError as e: 
-                print e
-                pass
-        
+            print synset
+            nyms = ['hypernyms', 'hyponyms', 'meronyms', 'holonyms', 'part_meronyms', 'sisterm_terms', 'troponyms', 'inherited_hypernyms']
+            for i in nyms:
+                try:
+                    print getattr(synset, i)()
+                except AttributeError as e: 
+                    print e
+                    pass
+
+        for lemma in (wn.lemmas(user_input)):
+            print lemma
+            lemmanyms = ['antonyms', 'derivationally_related_forms', 'pertainyms']
+            for y in lemmanyms:
+                try:
+                    print getattr(lemma, y)()
+                except AttribureError as e:
+                    print e
+                    pass
+
 
 getsynsets(user_input)
+
+
